6b8486059de8647a890389630c61cb66a1f8b2ab
Bryan Robbins <bryantrobbins@gmail.com>
2018-05-04 02:33:44 -0500

    Prints all mlsIds to console

--- extract.js
+++ extract.js
@@ -5,52 +5,63 @@ fixture `Search Listings`
 
 test('', async t => {
   // Load page
+  await Selector('#mapsearch-criteria-maxprice');
+
   await t
     .click('#buttonimage')
     .click('#mapsearch-viewtab-list');
 
-  var card = await Selector('.gallery-listingphoto');
-
-  // Filter for Madison County
-  await t
-    .typeText('#criteria-location-input', 'Madison, AL (County)')
-    .click(Selector('li').withText('Madison, AL (County)'));
-  card = await Selector('.gallery-listingphoto');
-
-  // Add Limestone County
-  await t
-    .typeText('#criteria-location-input', 'Limestone, AL (County)')
-    .click(Selector('li').withText('Limestone, AL (County)'));
-  card = await Selector('.gallery-listingphoto');
-
-
-  // Expand 'Refine Search' Panel
-  await t
-    .click('#mapsearch-criteria-header > a');
-  var criteria = await Selector('#mapsearch-criteria-maxprice');
+  await Selector('.gallery-listingphoto');
+  await Selector('#mapsearch-criteria-maxprice');
 
   // Filter for price and sqft
   await t
     .typeText('#mapsearch-criteria-maxprice', '240000')
     .typeText('#mapsearch-criteria-minsqft', '1800')
     .click('#mapsearch-criteria-maxsqft');
-
-  card = await Selector('.gallery-listingphoto');
+  await Selector('.gallery-listingphoto');
 
   // Filter acreage
   await t
     .click('#mapsearch-criteria-minacreage')
     .click(Selector('option').filter('[value="10890"]'));
-
-  card = await Selector('.gallery-listingphoto');
+  await Selector('.gallery-listingphoto');
 
   // Filter age
   await t
     .click('#mapsearch-criteria-age')
     .click(Selector('option').filter('[value="3929"]'));
+  await Selector('.gallery-listingphoto');
 
-  card = await Selector('.gallery-listingphoto');
-
+  // Filter for Madison County
   await t
-    .wait(10000);
+    .typeText('#criteria-location-input', 'Madison, AL (County)')
+    .click(Selector('li').withText('Madison, AL (County)'));
+  await Selector('.gallery-listingphoto');
+
+  // Add Limestone County
+  await t
+    .typeText('#criteria-location-input', 'Limestone, AL (County)')
+    .click(Selector('li').withText('Limestone, AL (County)'));
+  await Selector('.gallery-listingphoto');
+
+  // Extract MLS IDs from first page
+  const nResultInPage = await Selector('div.listview-infocontainer > div.listview-info > div:nth-child(5) > dd').count;
+  for (let i = 0; i < nResultInPage; i++) {
+    const mlsId = await Selector('div.listview-infocontainer > div.listview-info > div:nth-child(5) > dd').nth(i).textContent;
+    console.log(mlsId);
+  }
+
+  // Repeat extraction for remaining pages
+  const next = Selector('a').withText('Next');
+  while (await next.exists) {
+    await t.click(next);
+    await Selector('.gallery-listingphoto');
+    const nResultInPage = await Selector('div.listview-infocontainer > div.listview-info > div:nth-child(5) > dd').count;
+    for (let i = 0; i < nResultInPage; i++) {
+      const mlsId = await Selector('div.listview-infocontainer > div.listview-info > div:nth-child(5) > dd').nth(i).textContent;
+      console.log(mlsId);
+    }
+  }
+
 });
