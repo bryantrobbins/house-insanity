3a2351e65668a2811d600f4553f2af1a68bab1e6
Bryan Robbins <bryantrobbins@gmail.com>
2018-05-04 01:47:38 -0500

    Add age filter

--- extract.js
+++ extract.js
@@ -17,9 +17,13 @@ test('', async t => {
     .click(Selector('li').withText('Madison, AL (County)'));
   card = await Selector('.gallery-listingphoto');
 
+  // Expand 'Refine Search' Panel
+  await t
+    .click('#mapsearch-criteria-header > a');
+  var criteria = await Selector('#mapsearch-criteria-maxprice');
+
   // Filter for price and sqft
   await t
-    .click('#mapsearch-criteria-header > a')
     .typeText('#mapsearch-criteria-maxprice', '240000')
     .typeText('#mapsearch-criteria-minsqft', '1800')
     .click('#mapsearch-criteria-maxsqft');
@@ -30,6 +34,15 @@ test('', async t => {
   await t
     .click('#mapsearch-criteria-minacreage')
     .click(Selector('option').filter('[value="10890"]'));
+
+  card = await Selector('.gallery-listingphoto');
+
+  // Filter age
+  await t
+    .click('#mapsearch-criteria-age')
+    .click(Selector('option').filter('[value="3929"]'));
+
+  card = await Selector('.gallery-listingphoto');
 
   await t
     .wait(10000);
