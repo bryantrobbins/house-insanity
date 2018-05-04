00a4c7bfb8917e4acfbe9cdda46a7c42208aa055
Bryan Robbins <bryantrobbins@gmail.com>
2018-05-04 01:41:54 -0500

    Apply all filters

--- extract.js
+++ extract.js
@@ -4,15 +4,33 @@ fixture `Search Listings`
   .page `http://www.valleymls.com/`;
 
 test('', async t => {
+  // Load page
   await t
     .click('#buttonimage')
     .click('#mapsearch-viewtab-list');
-  const card = await Selector('.gallery-listingphoto');
+
+  var card = await Selector('.gallery-listingphoto');
+
+  // Filter for Madison County
   await t
     .typeText('#criteria-location-input', 'Madison, AL (County)')
-    .click(Selector('li').withText('Madison, AL (County)'))
+    .click(Selector('li').withText('Madison, AL (County)'));
+  card = await Selector('.gallery-listingphoto');
+
+  // Filter for price and sqft
+  await t
+    .click('#mapsearch-criteria-header > a')
     .typeText('#mapsearch-criteria-maxprice', '240000')
     .typeText('#mapsearch-criteria-minsqft', '1800')
-    .click('#mapsearch-viewtab-list')
-    .wait( 10000 );
+    .click('#mapsearch-criteria-maxsqft');
+
+  card = await Selector('.gallery-listingphoto');
+
+  // Filter acreage
+  await t
+    .click('#mapsearch-criteria-minacreage')
+    .click(Selector('option').filter('[value="10890"]'));
+
+  await t
+    .wait(10000);
 });
